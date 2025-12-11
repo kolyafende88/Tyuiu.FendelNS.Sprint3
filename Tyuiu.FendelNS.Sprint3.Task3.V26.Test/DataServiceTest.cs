c27c826842ed2f2ed7781a9ba20be0f1f43c160f
Tyuiu.FendelNS <Tyuiu.FendelNS@outlook.com>
2025-12-11 23:12:55 +0500

    Task3

---
+++ Tyuiu.FendelNS.Sprint3.Task3.V26.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.FendelNS.Sprint3.Task3.V26.Lib;
+namespace Tyuiu.FendelNS.Sprint3.Task3.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCharCount()
+        {
+            DataService ds = new DataService();
+
+            string str = "have a nice time";
+            char chr = 'e';
+
+            double res = ds.GetCharCount(str, chr);
+
+            int wait = 3;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.FendelNS.Sprint3.Task3.V26/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.FendelNS.Sprint3.Task3.V26.Lib;
+namespace Tyuiu.FendelNS.Sprint3.Task3.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Фендель Н. С. | СМАРТБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла foreach                                            *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнил: Фендель Николай  Сергеевич | СМАРТБ-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Используя цикл foreach подсчитать количество букв “e” в строке:         *");
+            Console.WriteLine("* have a nice time                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string value = "have a nice time";
+            char chr = 'e';
+
+            Console.WriteLine("Исходная строка = " + value);
+            Console.WriteLine("Исходный символ = " + chr);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Количество символов = " + ds.GetCharCount(value, chr));
+            Console.ReadKey();
+        }
+    }
+}
