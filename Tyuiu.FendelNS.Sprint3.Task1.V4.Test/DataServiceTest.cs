01a1f402bc7c678ece5d38b1694e888e47e34bb0
Tyuiu.FendelNS <Tyuiu.FendelNS@outlook.com>
2025-12-11 21:14:46 +0500

    Task1

---
+++ Tyuiu.FendelNS.Sprint3.Task1.V4.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.FendelNS.Sprint3.Task1.V4.Lib;
+namespace Tyuiu.FendelNS.Sprint3.Task1.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+            int startValue = 1;
+            int stopValue = 10;
+            Assert.AreEqual(1.55, ds.GetSumSeries(startValue, stopValue));
+        }
+    }
+}
---
+++ Tyuiu.FendelNS.Sprint3.Task1.V4/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.FendelNS.Sprint3.Task1.V4.Lib;
+namespace Tyuiu.FendelNS.Sprint3.Task1.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Фендель Н. С. | СМАРТб-24-1";
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Спринт #3                                                             *");
+            Console.WriteLine("* Тема: Оператор цикла while                                            *");
+            Console.WriteLine("* Задание #1                                                            *");
+            Console.WriteLine("* Варинат #4                                                            *");
+            Console.WriteLine("* Выполнил: Фендель Николай Сергеевич | СМАРТб-24-1                     *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* ИССХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("StartValue = 1");
+            Console.WriteLine("StopValue = 10");
+            int a, b;
+            a = 1;
+            b = 10;
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+
+            Console.WriteLine(ds.GetSumSeries(a, b));
+            Console.ReadKey();
+        }
+    }
+}
