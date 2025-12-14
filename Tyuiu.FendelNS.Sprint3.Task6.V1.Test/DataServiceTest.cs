e2154abd65e0b921de89b6a6cf56644b4b27d863
Tyuiu.FendelNS <Tyuiu.FendelNS@outlook.com>
2025-12-15 03:53:17 +0500

    Task6

---
+++ Tyuiu.FendelNS.Sprint3.Task6.V1.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.FendelNS.Sprint3.Task6.V1.Lib;
+namespace Tyuiu.FendelNS.Sprint3.Task6.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 11;
+            int stopValue = 17;
+
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+            int wait = 151;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.FendelNS.Sprint3.Task6.V1/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.FendelNS.Sprint3.Task6.V1.Lib;
+namespace Tyuiu.FendelNS.Sprint3.Task6.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Фендель Н. С. | СМАРТБ-24-1";          
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Обработка целочисленной информации                                *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Фендель Николай Сергеевич | СМАРТБ-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Напишите программу, которая ищет среди целых чисел, принадлежащих       *");
+            Console.WriteLine("* числовому отрезку [11, 17] сумму всех делителей.                        *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startValue = 11;
+            int stopValue = 17;
+
+
+            Console.WriteLine("Начало отрезка = " + startValue);
+            Console.WriteLine("Конец отрезка = " + stopValue);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма делителей = " + ds.GetSumTheDivisors(startValue, stopValue));
+
+            Console.ReadLine();
+        }
+    }
+}
